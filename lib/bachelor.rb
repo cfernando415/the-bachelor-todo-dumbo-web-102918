85dc138e83301eef9f74cb7ad6fda14c2a8773d6
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-17 04:20:46 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -49,7 +49,7 @@ def get_average_age_for_season(data, season)
   data.each do |seasons, contestants|
     if seasons ==  season
       contestants.each do |contestant|
-        sum_age += contestant["age"].to_f
+        sum_age += contestant["age"].to_i
       end
       i = contestants.length
     end
